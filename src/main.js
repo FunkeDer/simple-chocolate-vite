d08c13859e539161421bbc023d66033ad5eca114
Владислав Душко Iгорович <vladyslav.dusko@gmail.com>
2024-03-14 15:24:49 +0200

    mobile menu updates

--- src/main.js
+++ src/main.js
@@ -19,19 +19,34 @@ closeModal.addEventListener("click", () => {
   );
 });
 
-
-
 const mobileOpenButton = document.getElementById("mobile_open");
-const mobileCloseButton = document.getElementById("mobile_close");
+const mobileCloseButton = document.getElementById("close-button");
 const mobileContainer = document.getElementById("mobile_container");
 const mobileNavItem = document.getElementById("mobile_nav_item")
+const mobileMenuItem = document.querySelectorAll(".mobile-menu-item")
+const backdrop = document.querySelector(".mobile-menu-backdrop")
 
 mobileOpenButton.addEventListener('click', () => {
-  mobileContainer.style.cssText = 'display: flex; '; 
+  backdrop.classList.remove('visually-hidden');
+  mobileContainer.classList.add("mobile-menu-show"); 
+  mobileContainer.classList.remove("visually-hidden"); 
+  mobileContainer.classList.remove("mobile-menu-hidden");
+  document.body.classList.add('overflow-hidden'); 
 });
 
-function closeMobileMenu () {
-  mobileContainer.style.cssText = 'visibility: hidden; display:none;';
-}
-mobileCloseButton.addEventListener('click', closeMobileMenu)
+mobileCloseButton.addEventListener('click', () => {
+  backdrop.classList.add('visually-hidden');
+  mobileContainer.classList.add("mobile-menu-hidden");
+  setTimeout(function() {
+    mobileContainer.classList.add("visually-hidden");
+    mobileContainer.classList.remove("mobile-menu-show");
+}, 300); 
+document.body.classList.remove('overflow-hidden'); 
+  })
 
+backdrop.addEventListener("click", () => {
+  mobileContainer.classList.add("mobile-menu-hidden");
+  mobileContainer.classList.add("visually-hidden"); 
+  mobileContainer.classList.remove("mobile-menu-show");
+  document.body.classList.remove('overflow-hidden'); 
+})
